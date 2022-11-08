13384dfc0f1c6270c156a95bb7e8e175c22baf9f
erikaArango <enfermeraerikaarango@gmail.com>
2022-11-08 10:31:46 -0300

    Header component and Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Breeds from './components/Breeds';
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="container">
-      <BrowserRouter>
+        <BrowserRouter>
+        <div>
+        <Header />
+        </div>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/breeds' element ={<Breeds></Breeds>}></Route>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import perros from '../img/perros.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h2>   Are you ready to know all dog breeds and sub-breeds with a click?</h2>
+      <img   src={perros} alt="" className='dogs' />
+    </div>
   )
 }
 
